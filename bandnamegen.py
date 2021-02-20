abf6598abb6ec94c46c5294d4a490e85b92e281f
Chad Parker <63312621+ChimpBite@users.noreply.github.com>
2021-02-19 21:53:54 -0700

    Finished

---
+++ bandnamegen.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name genaerator")
+city = input("Which city did you grow up in?\n")
+pet = input("What is your favorit paets name?\n")
+print("Your band name could be " + city + " " + pet)
+
